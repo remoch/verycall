ba5bd2ddd330ff217502f34c3776e9f900036d8b
remoch <remathis@proton.me>
2024-12-17 15:12:19 +0100

    Update twilioService.js

--- src/services/twilioService.js
+++ src/services/twilioService.js
@@ -3,14 +3,27 @@ const hederaService = require('./hederaService');
 
 const VoiceResponse = twilio.twiml.VoiceResponse;
 
+// Whitelist of allowed numbers
+const ALLOWED_NUMBERS = {
+    '1': '1234567890', // Example: Amazon Customer Service
+    '2': '00000000000', // Example: Test Number 2
+    '3': '5555555555', // Example: Test Number 3
+};
+
 class TwilioService {
     handleIncomingCall(req, res) {
         const twiml = new VoiceResponse();
         
+        // Provide menu of options
+        twiml.say('Welcome to the call logger. Please select from the following options:');
+        twiml.say('Press 1 for Amazon Customer Service');
+        twiml.say('Press 2 for Test Number 2');
+        twiml.say('Press 3 for Test Number 3');
+        
         twiml.gather({
-            numDigits: 10,
+            numDigits: 1,
             action: '/process-call'
-        }).say('Please enter the phone number you wish to call');
+        });
 
         res.type('text/xml');
         res.send(twiml.toString());
@@ -18,8 +31,16 @@ class TwilioService {
 
     async processCall(req, res) {
         const callerNumber = req.body.From;
-        const calledNumber = req.body.Digits;
-        
+        const selection = req.body.Digits;
+        const calledNumber = ALLOWED_NUMBERS[selection];
+
+        if (!calledNumber) {
+            const twiml = new VoiceResponse();
+            twiml.say('Invalid selection. Goodbye.');
+            res.type('text/xml');
+            return res.send(twiml.toString());
+        }
+
         // Log to Hedera
         const callData = {
             caller: callerNumber,
@@ -29,22 +50,17 @@ class TwilioService {
         };
 
         try {
-            await hederaService.logCall(callData);
+            const hederaResponse = await hederaService.logCall(callData);
             
-            // Send SMS confirmation
-            const client = twilio(
-                process.env.TWILIO_ACCOUNT_SID,
-                process.env.TWILIO_AUTH_TOKEN
-            );
-
-            await client.messages.create({
-                body: `Call logged. From: ${callerNumber} To: ${calledNumber}`,
-                to: callerNumber,
-                from: process.env.TWILIO_PHONE_NUMBER
-            });
-
-            // Connect the call
+            // Provide verbal verification code
+            const verificationCode = hederaResponse.hashedData.callId.substring(0, 6);
+            
             const twiml = new VoiceResponse();
+            twiml.say(`Your verification code is ${verificationCode.split('').join(' ')}`);
+            twiml.say('Please write this down. You can verify your call at our website using this code.');
+            twiml.pause({ length: 2 });
+            
+            // Connect the call
             twiml.dial(calledNumber);
             
             res.type('text/xml');
